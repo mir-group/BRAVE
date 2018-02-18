d9bee77b1766533634116be550dab9d4b66debb8
Georgy Samsonidze <georgy.samsonidze@us.bosch.com>
2018-02-18 11:18:56 -0500

    Replaced numpy with np.

--- test/kpath/kpath_rot.py
+++ test/kpath/kpath_rot.py
@@ -2,15 +2,15 @@
 # rotates them to the lattice vectors used by Quantum ESPRESSO (ibrav = 2 & 3),
 # and writes them to files fcc_rot.full.in and bcc_rot.full.in.
 
-import numpy
+import numpy as np
 import brave
 
 kpt = brave.Kpoint()
 kpt.read('internal', ['fcc.full.in'])
 del kpt.bvec
-avec_rot = numpy.array([[-0.5, 0.0, 0.5], [0.0, 0.5, 0.5], [-0.5, 0.5, 0.0]], float)
-mtx = numpy.dot(avec_rot, numpy.linalg.inv(kpt.avec))
-kpath_rot = numpy.transpose(numpy.dot(mtx, numpy.transpose(kpt.kpath)))
+avec_rot = np.array([[-0.5, 0.0, 0.5], [0.0, 0.5, 0.5], [-0.5, 0.5, 0.0]], float)
+mtx = np.dot(avec_rot, np.linalg.inv(kpt.avec))
+kpath_rot = np.transpose(np.dot(mtx, np.transpose(kpt.kpath)))
 kpt.avec = avec_rot
 kpt.kpath = kpath_rot
 kpt.write('internal', ['fcc_rot.full.in'])
@@ -18,9 +18,9 @@ kpt.write('internal', ['fcc_rot.full.in'])
 kpt = brave.Kpoint()
 kpt.read('internal', ['bcc.full.in'])
 del kpt.bvec
-avec_rot = numpy.array([[0.5, 0.5, 0.5], [-0.5, 0.5, 0.5], [-0.5, -0.5, 0.5]], float)
-mtx = numpy.dot(avec_rot, numpy.linalg.inv(kpt.avec))
-kpath_rot = numpy.transpose(numpy.dot(mtx, numpy.transpose(kpt.kpath)))
+avec_rot = np.array([[0.5, 0.5, 0.5], [-0.5, 0.5, 0.5], [-0.5, -0.5, 0.5]], float)
+mtx = np.dot(avec_rot, np.linalg.inv(kpt.avec))
+kpath_rot = np.transpose(np.dot(mtx, np.transpose(kpt.kpath)))
 kpt.avec = avec_rot
 kpt.kpath = kpath_rot
 kpt.write('internal', ['bcc_rot.full.in'])
