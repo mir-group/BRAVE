58f028e56675029f11eb7df00382d23e43c457ad
Georgy Samsonidze <Georgy.Samsonidze@us.bosch.com>
2020-09-17 22:18:33 -0700

    Reran QE, BGW, BoltzTraP tests

--- test/silicon/2_bgw/3_inteqp/plot_bands.py
+++ test/silicon/2_bgw/3_inteqp/plot_bands.py
@@ -4,10 +4,6 @@ bnd = brave.Diagram()
 bnd.read('internal', ['../../../kpath/fcc.full.in'])
 bnd.calc_kindex('density')
 bnd.read('inteqp-out', ['../../1_qe/1_scf/1.pw.out', 'bandstructure.dat'])
-
-# check that first k-point is Gamma
-bnd.kpoint[0, :] = 0
-
 bnd.calc_efermi()
 bnd.set_plot('energy')
 bnd.plot.ylim = [[0, 12]]
